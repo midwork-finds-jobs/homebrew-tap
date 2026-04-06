a53bc9c2be41c788c42c093d2ac85151cc6ae7e1
Onni Hakala <onni@flaky.build>
2026-04-06 22:32:20 +0300

    feat(hrobot): bump to v0.6.0

--- Formula/hrobot.rb
+++ Formula/hrobot.rb
@@ -1,26 +1,26 @@
 class Hrobot < Formula
   desc "CLI for Hetzner Robot API - manage dedicated servers, firewall, SSH keys"
   homepage "https://github.com/midwork-finds-jobs/terraform-provider-hrobot"
-  version "0.5.0"
+  version "0.6.0"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.5.0/hrobot-darwin-arm64.tar.gz"
-      sha256 "5d6fc9c5db6e73ff583ebf16b96e4fef6eb3509881aeb1a622ac73cbd8c089f5"
+      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.6.0/hrobot-darwin-arm64.tar.gz"
+      sha256 "0139f61f726308e3247976b720ab65ccb60d92de84bdd3e926b3db42951a95b7"
     else
-      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.5.0/hrobot-darwin-amd64.tar.gz"
-      sha256 "196c5bbb2bb3896dd20f416d95b122a0b3af04586898bc8e72f1ebdc1f1be9ef"
+      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.6.0/hrobot-darwin-amd64.tar.gz"
+      sha256 "3617de07ff13736a83384c91d98fbcc8eb7e5eff382302608c5c845cadb4f0ec"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.5.0/hrobot-linux-arm64.tar.gz"
-      sha256 "3afe7e06f38538a6cf2b98ac895a849e3d42a77241b0ce17f6faaa253754e04d"
+      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.6.0/hrobot-linux-arm64.tar.gz"
+      sha256 "07179e3241512c019d15d31e275f2a3e2a5b44e3945781ab722187be59857a7c"
     else
-      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.5.0/hrobot-linux-amd64.tar.gz"
-      sha256 "a9cbaa692ae3b6f12a870af508db9f974590a1a1b30f16204156136936dd6680"
+      url "https://github.com/midwork-finds-jobs/terraform-provider-hrobot/releases/download/v0.6.0/hrobot-linux-amd64.tar.gz"
+      sha256 "0b5cb2da2bd71631d9dc42c2a89b846777e86447959d9103ae8ddf741d4fb4ff"
     end
   end
 
